eaa6155005286ef08340357ef8406d63dfcbd9c0
prmiguel <prmiguel-pr@outlook.com>
2025-12-29 13:50:25 -0400

    No weston

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,6 @@
 
 docker rm -f waydroidu
 docker build -f Dockerfile.ubuntu -t waydroidu .
-docker run -d --rm --name waydroidu --privileged --shm-size=1g -v /lib/modules:/lib/modules:ro -v /var/lib/waydroid:/var/lib/waydroid -p 3001:3001 waydroid
+docker run -d --rm --name waydroidu --privileged --shm-size=1g -v /lib/modules:/lib/modules:ro -v /var/lib/waydroid:/var/lib/waydroid -p 3001:3001 -e PIXELFLUX_WAYLAND=true waydroidu
 docker exec -it --user=abc waydroidu bash
 start-waydroid
